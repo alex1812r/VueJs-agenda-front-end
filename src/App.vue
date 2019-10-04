21bf82f4372abee0dfaefe4c6660e11d470b793e
Alex1812r <alex1812r@gmail.com>
2019-10-04 18:25:06 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,241 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">AGENDA</a>
+    </nav>
+    <div class="container-fluid">
+      <div class="row pt-5">
+        
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <form @submit.prevent="enviarContacto" >
+                
+                <div class="form-group row">
+                  <div class="col-md-6">
+                    <input
+                    v-model="contacto.nombre"
+                    type="text" 
+                    placeholder="Nombre"
+                    class="form-control mb-sm-3 mb-md-0">
+                  </div>
+                  <div class="col-md-6">
+                    <input
+                    v-model="contacto.apellido"
+                    type="text" 
+                    placeholder="Apellido"
+                    class="form-control">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <div class="col-md-9">
+                    <input 
+                      v-model="contacto.telefono"
+                      type="text"
+                      placeholder="Telefono"
+                      class="form-control"
+                      >
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <input 
+                    v-model="contacto.correo"
+                    type="email"
+                    placeholder="Email"
+                    class="form-control"
+                  >
+                </div>
+
+                <div class="form-group">
+                  <textarea
+                    v-model="contacto.direccion" 
+                    rows="4" 
+                    class="form-control" 
+                    placeholder="Direccion">
+
+                  </textarea>
+                </div>
+                
+                <template v-if="editar === false">
+                  <button class="btn btn-success btn-block">
+                    Enviar
+                  </button>
+                </template>
+
+                <template v-else>
+                  <button class="btn btn-primary btn-block">
+                    Editar
+                  </button>
+                </template>
+
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-8">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+                <th>Direccion</th>
+                <th colspan="2" style="text-align:center">Opciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(contacto,key) of contactos" v-bind:key="key">
+                <td> {{ contacto.nombre }} </td>
+                <td> {{ contacto.apellido }} </td>
+                <td> {{ contacto.telefono }} </td>
+                <td> {{ contacto.correo }} </td>
+                <td> {{ contacto.direccion }} </td>
+                <td>
+                  <button 
+                    type="button" 
+                    @click="editarContacto(contacto._id)" 
+                    class="btn btn-primary">
+                    <i class="far fa-edit"></i>
+                  </button>
+                </td>
+                <td>
+                  <button 
+                    type="button" 
+                    @click="eliminarContacto(contacto._id)" 
+                    class="btn  btn-danger">
+                    <i class="fas fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  class Contacto {
+    constructor(
+      nombre, apellido, telefono, 
+      correo, direccion
+    ) 
+    {
+      this.nombre = nombre
+      this.apellido = apellido
+      this.telefono = telefono
+      this.correo = correo
+      this.direccion = direccion
+    }
   }
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  export default {
+
+    data() {
+      return {
+        contacto: new Contacto(),
+        contactos: [],
+        editar: false,
+        contactoAEditar: ''
+      }
+    },
+    //realiza una funcion antes de cargar el componente
+    created(){
+      this.obtenerContactos()
+    },
+
+    methods: {
+
+      obtenerContactos (){
+        fetch('/api/agenda',)
+        .then( res => res.json() )
+        .then( data => {
+          //console.log('data :', data)
+          this.contactos = data
+        })
+      },
+
+      enviarContacto() {
+        if(!this.editar){
+
+          fetch('/api/agenda',{
+            method: 'POST',
+            body: JSON.stringify(this.contacto),
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+          })
+            .then( res => res.json())
+            .then( () => {
+              //console.log('data :', data)
+              this.obtenerContactos()
+            })
+
+        }else{
+
+          fetch('/api/agenda/' + this.contactoAEditar,{
+            method: 'PUT',
+            body: JSON.stringify(this.contacto),
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+          })
+            .then( res => res.json() )
+            .then( () => {
+              this.obtenerContactos()
+            })
+
+          this.contactoAEditar = ''
+          this.editar = false
+        }
+
+        this.contacto = new Contacto()
+      },
+
+      eliminarContacto(id) {
+        const valid = confirm('confirme si desea eliminar el contacto')
+        if(valid){
+
+          fetch('api/agenda/' + id,{
+            method:'DELETE',
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+          })
+            .then( res => res.json() )
+            .then( () => {
+              this.obtenerContactos()
+            })
+
+        }
+      },
+
+      editarContacto(id){
+        //console.log('object :', id)
+        fetch('/api/agenda/' + id)
+          .then( res => res.json() )
+          .then( data => {
+            //console.log('data :', data)
+            this.contacto = new Contacto(
+              data.nombre, data.apellido, data.telefono,
+              data.correo, data.direccion
+            )
+
+            this.contactoAEditar = data._id
+            this.editar = true
+          })
+      }
+
+    }  
+  }
+</script>
